c8e0575eb552dde79e18c8c5e12547cba6107db0
Matheus Freitas <matheusmfs@hotmail.com>
2020-03-08 16:32:16 -0300

    Adicionado o campo CPF, adicionada a verificacao de CPF no controller

---
+++ FI.WebAtividadeEntrevista/Controllers/ClienteController.cs
@@ -0,0 +1,168 @@
+﻿using FI.AtividadeEntrevista.BLL;
+using WebAtividadeEntrevista.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FI.AtividadeEntrevista.DML;
+using FI.AtividadeEntrevista.DAL; 
+
+namespace WebAtividadeEntrevista.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Incluir()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Incluir(ClienteModel model)
+        {
+            BoCliente bo = new BoCliente();
+            
+
+
+            if (!this.ModelState.IsValid)
+            {
+                List<string> erros = (from item in ModelState.Values
+                                      from error in item.Errors
+                                      select error.ErrorMessage).ToList();
+
+                Response.StatusCode = 400;
+                return Json(string.Join(Environment.NewLine, erros));
+            }
+            else
+            {
+
+                
+                System.Diagnostics.Debug.WriteLine("CPF COM ERRO: " + model.CPF);
+                System.Diagnostics.Debug.WriteLine("CEP OK: " + model.CEP);
+
+
+
+                if (!bo.VerificarExistencia(model.CPF)){
+                    
+                    model.Id = bo.Incluir(new Cliente()
+                    {
+                        Nome = model.Nome,
+                        Sobrenome = model.Sobrenome,
+                        CPF = model.CPF,
+                        Nacionalidade = model.Nacionalidade,
+                        CEP = model.CEP,
+                        Estado = model.Estado,
+                        Cidade = model.Cidade,
+                        Logradouro = model.Logradouro,
+                        Email = model.Email,
+                        Telefone = model.Telefone
+                    });
+
+                    return Json("Cadastro efetuado com sucesso");
+
+                } else
+                {
+                   return Json("CPF Já Cadastrado!");
+                }
+            }
+        }
+
+        [HttpPost]
+        public JsonResult Alterar(ClienteModel model)
+        {
+            BoCliente bo = new BoCliente();
+       
+            if (!this.ModelState.IsValid)
+            {
+                List<string> erros = (from item in ModelState.Values
+                                      from error in item.Errors
+                                      select error.ErrorMessage).ToList();
+
+                Response.StatusCode = 400;
+                return Json(string.Join(Environment.NewLine, erros));
+            }
+            else
+            {
+                bo.Alterar(new Cliente()
+                {
+                    CPF = model.CPF,
+                    Id = model.Id,
+                    CEP = model.CEP,
+                    Cidade = model.Cidade,
+                    Email = model.Email,
+                    Estado = model.Estado,
+                    Logradouro = model.Logradouro,
+                    Nacionalidade = model.Nacionalidade,
+                    Nome = model.Nome,
+                    Sobrenome = model.Sobrenome,
+                    Telefone = model.Telefone
+                });
+                               
+                return Json("Cadastro alterado com sucesso");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Alterar(long id)
+        {
+            BoCliente bo = new BoCliente();
+            Cliente cliente = bo.Consultar(id);
+            Models.ClienteModel model = null;
+
+            if (cliente != null)
+            {
+                model = new ClienteModel()
+                {
+                    CPF = cliente.CPF,
+                    Id = cliente.Id,
+                    CEP = cliente.CEP,
+                    Cidade = cliente.Cidade,
+                    Email = cliente.Email,
+                    Estado = cliente.Estado,
+                    Logradouro = cliente.Logradouro,
+                    Nacionalidade = cliente.Nacionalidade,
+                    Nome = cliente.Nome,
+                    Sobrenome = cliente.Sobrenome,
+                    Telefone = cliente.Telefone
+                };
+
+            
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public JsonResult ClienteList(int jtStartIndex = 0, int jtPageSize = 0, string jtSorting = null)
+        {
+            try
+            {
+                int qtd = 0;
+                string campo = string.Empty;
+                string crescente = string.Empty;
+                string[] array = jtSorting.Split(' ');
+
+                if (array.Length > 0)
+                    campo = array[0];
+
+                if (array.Length > 1)
+                    crescente = array[1];
+
+                List<Cliente> clientes = new BoCliente().Pesquisa(jtStartIndex, jtPageSize, campo, crescente.Equals("ASC", StringComparison.InvariantCultureIgnoreCase), out qtd);
+
+                //Return result to jTable
+                return Json(new { Result = "OK", Records = clientes, TotalRecordCount = qtd });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { Result = "ERROR", Message = ex.Message });
+            }
+        }
+    }
+}
